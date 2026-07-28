db2eec8a624fdd488f08f76917325140726aaf9a
Deepanshu Pandey <deepanshupandey046@gmail.com>
2026-07-28 22:50:36 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.4%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int temp =0;
+        int j = -1;
+        int n =nums.size();
+        for(int i = 0 ; i<n ; i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        if(j==-1) {
+
+        }      
+        else{
+            for(int i = j+1 ; i<n ; i++){
+            if(nums[i]!=0){
+                swap(nums[i] , nums[j]);
+                j++;
+            }
+        }
+        
+        }
+        
+    }
+};
