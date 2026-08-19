c1d07b7fb5448d3224df888114b85439e291dc79
Deepanshu Pandey <deepanshupandey046@gmail.com>
2026-08-19 18:21:58 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.11%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int sol=0;
-        for( int item : nums){
-            sol^=item;
-        }
+       int sol = 0 ;
+       for(int item:nums){
+        sol^=item;
+       }
     return sol;
 }
 };
